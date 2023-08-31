13e4c6a0ccf36fa14d0e6679e7412cc556e62290
Elise Ngo <elisetvy@gmail.com>
2023-08-30 17:03:15 -0700

    Write unit tests

--- tests.py
+++ tests.py
@@ -5,7 +5,7 @@ os.environ["DATABASE_URL"] = "postgresql:///blogly_test"
 from unittest import TestCase
 
 from app import app, db
-from models import DEFAULT_IMAGE_URL, User
+from models import User
 
 # Make Flask errors be real errors, rather than HTML pages with error info
 app.config['TESTING'] = True
@@ -60,3 +60,49 @@ class UserViewTestCase(TestCase):
             html = resp.get_data(as_text=True)
             self.assertIn("test1_first", html)
             self.assertIn("test1_last", html)
+
+    def test_user_form(self):
+        """Test if new user page renders and contains form."""
+        with self.client as c:
+            resp = c.get("/users/new")
+            html = resp.get_data(as_text=True)
+
+            self.assertEqual(resp.status_code, 200)
+            self.assertIn('<h1>Create a user</h1>', html)
+            self.assertIn("Add", html)
+
+    def test_user_details(self):
+        """Test if user detail page renders."""
+        with self.client as c:
+            resp = c.get(f"/users/{self.user_id}")
+            html = resp.get_data(as_text=True)
+
+            self.assertEqual(resp.status_code, 200)
+            self.assertIn('<img', html)
+            self.assertIn('Edit', html)
+            self.assertIn("Delete", html)
+
+    def test_add_user(self):
+        """Tests add user button; checks if redirect occurs."""
+        with self.client as c:
+            resp = c.post('/users/new', data={
+                'first-name': 'Jay',
+                'last-name': 'Catsby',
+                'image-url': None
+            })
+
+            self.assertEqual(resp.status_code, 302)
+            self.assertEqual(resp.location, '/users')
+
+    def test_add_user_redirect(self):
+        """Test if new user has been added to user list."""
+        with self.client as c:
+            resp = c.post('/users/new', data={
+                'first-name': 'Jay',
+                'last-name': 'Catsby',
+                'image-url': None
+            }, follow_redirects=True)
+            html = resp.get_data(as_text=True)
+
+            self.assertEqual(resp.status_code, 200)
+            self.assertIn('Jay Catsby', html)
